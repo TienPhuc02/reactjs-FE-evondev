9d7f2edeaae84b829ce824fa7ee1780bd9046d8c
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-07 08:06:13 +0700

    react hook form cơ bản

--- useForm/src/SignUpFormHook/index.jsx
+++ useForm/src/SignUpFormHook/index.jsx
@@ -1,7 +1,13 @@
+import { useForm } from "react-hook-form";
 const SignUpFormHook = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <>
       <form
+        onSubmit={handleSubmit(onSubmit)}
         className="container-form p-10 w-full max-w-[500px] mx-auto"
         autoComplete="off"
       >
@@ -11,6 +17,8 @@ const SignUpFormHook = () => {
           </label>
           <input
             type="text"
+            defaultValue={"phuc"}
+            {...register("firstName")}
             name="firstName"
             className="p-4 rounded-md border border-gray-100 mb-4"
             id="firstName"
@@ -26,19 +34,21 @@ const SignUpFormHook = () => {
             name="lastName"
             className="p-4 rounded-md border border-gray-100 mb-4"
             id="lastName"
+            {...register("lastName")}
             placeholder="Enter Your lastName"
           />
         </div>
         <div className="flex flex-col gap-2">
-          <label htmlFor="lastName" className="cursor-pointer">
-            Last Name
+          <label htmlFor="email" className="cursor-pointer">
+            Email
           </label>
           <input
             type="text"
-            name="lastName"
+            name="email"
             className="p-4 rounded-md border border-gray-100 mb-4"
-            id="lastName"
-            placeholder="Enter Your lastName"
+            id="email"
+            {...register("email")}
+            placeholder="Enter Your email"
           />
         </div>
         <button
