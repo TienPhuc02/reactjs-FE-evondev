daf664a68cdf7857dfad6d5f23ff6167ff324a85
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-07 09:14:39 +0700

    sử dụng yup với react hook form

--- useForm/src/SignUpFormHook/index.jsx
+++ useForm/src/SignUpFormHook/index.jsx
@@ -1,10 +1,19 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yub from "yup";
+const schemaValidation = Yub.object({
+  firstName: Yub.string()
+    .required("Please fill on this field")
+    .max(5, "Must be 10 character or less"),
+});
 const SignUpFormHook = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(schemaValidation),
+  });
   console.log("🚀 ~ file: index.jsx:4 ~ SignUpFormHook ~ errors:", errors);
   const onSubmit = (values) => {
     console.log(values);
@@ -19,29 +28,30 @@ const SignUpFormHook = () => {
       >
         <div className="flex flex-col gap-2">
           <label htmlFor="firstName" className="cursor-pointer">
-            Username
+            First Name
           </label>
           <input
             type="text"
-            {...register("firstName", {
-              required: true,
-              maxLength: 10,
-              minLength: 2,
-              // pattern regex
-            })}
+            {...register("firstName")}
+            // {...register("firstName", {
+            //   required: true,
+            //   maxLength: 10,
+            //   minLength: 2,
+            //   // pattern regex
+            // })}
             name="firstName"
             className="p-4 rounded-md border border-gray-100 mb-4"
             id="firstName"
             placeholder="Enter Your firstName"
           />
-          {errors?.firstName?.type === "required" && (
+          {errors?.firstName?.type === "max" && (
             <div className="text-red-500 text-sm mb-3">
-              Must be 10 character or less
+              {errors?.firstName?.message}
             </div>
           )}
-          {errors?.firstName?.type === "maxLength" && (
+          {errors?.firstName?.type === "required" && (
             <div className="text-red-500 text-sm mb-3">
-              Please fill on this field
+              {errors?.firstName?.message}
             </div>
           )}
         </div>
